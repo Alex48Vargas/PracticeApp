14a9468f545bda264c75e85b71754ec3e5aea58c
Alex Vargas <alex48vargas@gmail.com>
2023-08-07 15:12:17 -0400

    Section 12

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -4,10 +4,20 @@ namespace API.DTOs
 {
     public class RegisterDTO
     {
-        [Required]
+        [Required] 
         public string Username { get; set; }
-        [Required]
-        [StringLength (8, MinimumLength = 4)]
+        [Required] 
+        public string KnownAs { get; set; }
+        [Required] 
+        public string Gender { get; set; }
+        [Required] 
+        public DateOnly? DateOfBirth { get; set; } // optional for required to work?
+        [Required] 
+        public string City { get; set; }
+        [Required] 
+        public string Country { get; set; }
+
+        [Required] [StringLength (8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,26 +1,71 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  // FORM METHODS
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPass: ['', [Validators.required, this.matchValues('password')]],
+      gender: ['male'],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required]
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPass'].updateValueAndValidity()
+    });
+
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
+  }
+
+  // OTHER METHODS
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: res => this.cancel(),
-      error: err => this.toastr.error(err.error)
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: dob};
+    this.accountService.register(values).subscribe({
+      next: res => this.router.navigateByUrl('/members'),
+      error: err => this.validationErrors = err
     })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+    let date = new Date(dob);
+    return new Date(date.setMinutes(date.getMinutes()-date.getTimezoneOffset())).toISOString().slice(0, 10);
   }
 }
